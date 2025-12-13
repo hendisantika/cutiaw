70006e80fea5339367f1e9564b0cd63fa360aa3b
hendisantika <hendisantika@yahoo.co.id>
2025-12-13 12:14:39 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/cutiaw/CutiawApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.cutiaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutiawApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CutiawApplication.class, args);
+    }
+
+}
